19297b24a3fecf8bf3dc99dd40ccbcf9f5a52061
myburt <myburt@hotmail.com>
2018-01-07 23:51:23 -0600

    Base Database setyp. Login page complete

--- main.py
+++ main.py
@@ -1,24 +1,63 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session,flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:SmallWord@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:SmallWord@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+
+app.secret_key = "m~kmSUK>:(j0f)4|wbP(.&CLZ6XWGU"
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(120))
     content = db.Column(db.String(1000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
 
-    def __init__(self, title, content):
+    def __init__(self, title, content, owner_id):
         self.title = title
         self.content = content
+        self.owner_id = owner_id
         
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(32))
+    password = db.Column(db.String(32))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init___(self, username, password):
+        self.username = username
+        self.password = password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register', 'list_blogs', 'index']
+    if request.endpoint not in allowed_routes and 'the_user' not in session:
+        return redirect('/login')
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html',title="Build-a-Blog!")
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            session['the_user'] = username
+            flash("Logged in", "info")
+            return redirect('/newpost')
+        elif user == None:
+            flash("username does not exist", "error")
+        else:
+            flash('User password incorrect', 'error')
+
+    return render_template('login.html')
 
 @app.route('/blog', methods=['GET', 'POST'])
 def blog_list():
